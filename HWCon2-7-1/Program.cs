c2ebaf7bd296c300865efa4bfb41be40296da973
GenZProg <GenZ.prog@outlook.com>
2021-12-19 13:27:03 +0800

    Исправление переменных балов

--- HWCon2-7-1/Program.cs
+++ HWCon2-7-1/Program.cs
@@ -12,22 +12,22 @@ namespace HWCon2_7_1
         {
             string fullName = "Иванов Иван Иванович", email = "IvanII@mail.ru";
             byte age = 27;
-            double ballProg = 33.31;
-            double ballMath = 70.98;
-            double ballPhys = 53.64;
+            double scorProg = 33.31;
+            double scorMath = 70.98;
+            double scorPhys = 53.64;
             string pattern = "ФИО: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
 
-            double mediumBall;
-            double sumBall;
+            double mediumScor;
+            double sumScor;
 
-            mediumBall = (ballProg + ballMath + ballPhys) / 3;
-            sumBall = ballProg + ballMath + ballMath;
-            string mediumBallFaorm = mediumBall.ToString("#.##");
+            mediumScor = (scorProg + scorMath + scorPhys) / 3;
+            sumScor = scorProg + scorMath + scorPhys;
+            string mediumBallFaorm = mediumScor.ToString("#.##");
 
-            Console.WriteLine(pattern, fullName, age, email, ballProg, ballMath, ballPhys);
+            Console.WriteLine(pattern, fullName, age, email, scorProg, scorMath, scorPhys);
             Console.ReadKey();
 
-            Console.WriteLine($"Сумма баллов: {sumBall}");
+            Console.WriteLine($"Сумма баллов: {sumScor}");
             Console.WriteLine($"Средний балл: {mediumBallFaorm}");
             Console.ReadKey();
 
